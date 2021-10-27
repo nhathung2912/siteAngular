042112353670fcc96abb503a7cac410041473a58
nhathung2912 <hungc4k43@gmail.com>
2021-10-27 18:35:39 +0200

    27/10

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+    public name: NamedCurve;
+    public amount: number;
+
+    constructor(name: string, amount: number) {
+        this.name = name;
+        this.amount = amount;
+
+    }
+}
